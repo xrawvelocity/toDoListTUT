8c7e5de61d1acc2f2bb6ec943cb2eca351376a1e
Victor Fernandez <vmfs99@gmail.com>
2020-03-04 17:17:55 -0500

    test

--- app.js
+++ app.js
@@ -129,6 +129,6 @@ if (port == null || port == "") {
   port = 3000;
 }
 
-app.listen(port, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
